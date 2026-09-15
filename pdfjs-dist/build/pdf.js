6fa3a258afccaefab0db41704989f6d0fd099ee8
Lukas Scharmer <lscharmer@databay.de>
2026-09-15 12:26:51 +0200

    Remove color picker in popup

--- pdfjs-dist/build/pdf.js
+++ pdfjs-dist/build/pdf.js
@@ -28423,8 +28423,7 @@ class HighlightEditor extends AnnotationEditor {
         editor: this
       });
       // edutiek-patch: begin
-      return this.#isFreeHighlight ? [["colorPicker", colorPicker], ['edutiek-token']] : [["colorPicker", colorPicker], ['edutiek-token'], ['edutiek']];
-      return this.#isFreeHighlight ? [["colorPicker", colorPicker]] : [["colorPicker", colorPicker], ['edutiek-token'], ['edutiek']];
+      return this.#isFreeHighlight ? [['edutiek-token']] : [['edutiek-token'], ['edutiek']];
       // edutiek-patch: end
     }
     return super.toolbarButtons;
